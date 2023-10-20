fcbdbb36f6d23b6c9afb70100c8bab7089d49029
Velpro <1693223420@qq.com>
2023-10-20 14:41:29 +0800

    新建项目

---
+++ src/main/java/org/wallace/Main.java
@@ -0,0 +1,14 @@
+package org.wallace;
+
+/**
+ * ClassName: ${NAME}
+ * Package: org.wallace
+ * Description:
+ *
+ * @Author yajuxi
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
